00639eb19aa6b45d2f1fbbbeabeb94b1e61d9388
abdelkarim <abdelkarim@iut-dhaka.edu>
2023-08-28 15:41:34 +0100

    Mui Layouts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Sidebar from './components/Sidebar'
 import Feed from './components/Feed'
 import Rightbar from './components/Rightbar'
+import { Box, Container, Stack } from '@mui/material';
 
 function App() {
   return (
-    <div>
-<Sidebar/>
+    <Box>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+      <Sidebar/>
 <Feed/>
 <Rightbar/>
-    </div>
+      </Stack>
+
+    </Box>
   );
 }
 
