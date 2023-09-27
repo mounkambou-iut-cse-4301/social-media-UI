832fd16a8ec4033172b9ff637d651d285ac55f25
abdelkarim <abdelkarim@iut-dhaka.edu>
2023-09-27 15:19:17 +0100

    Mui Modal Tutorial

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,12 +1,65 @@
-import { Fab, Tooltip } from '@mui/material';
-import { Add as AddIcon } from '@mui/icons-material';
-
+import { Avatar, Box, Button, ButtonGroup, Fab, Modal, Stack, TextField, Tooltip, Typography } from '@mui/material';
+import { Add as AddIcon, DateRange, EmojiEmotions, Image, PersonAdd, VideoCameraBack } from '@mui/icons-material';
+import { useState } from 'react';
+import styled from '@emotion/styled';
+const StyledModal=styled(Modal)({
+    display:"flex",
+    alignItems:"center",
+    justifyContent:"center"
+})
+const UserBox=styled(Box)({
+    display:"flex",
+    alignItems:"center",
+    gap:"10px",
+    marginBottom:"20px"
+})
 function Add() {
+    const [open,setOpen]=useState(false)
   return (
   <>
-  <Tooltip title="Add" sx={{position:"fixed",bottom:20, left:{xs:"calc(50%-25px)",md:30}}}><Fab color='primary' aria-label='add'>
+  <Tooltip 
+  onClick={e=>setOpen(true)}
+  title="Add" sx={{position:"fixed",bottom:20, left:{xs:"calc(50%-25px)",md:30}}}><Fab color='primary' aria-label='add'>
     <AddIcon/>
     </Fab></Tooltip>
+    <StyledModal
+    open={open}
+    onClose={e=>setOpen(false)}
+    arial-labelledby="modal-modal-title"
+    arial-describedby="modal-modal-description">
+        <Box width={400} height={280} bgcolor="white"
+        p={3}
+        borderRadius={5}
+        >
+           <Typography variant='h6' color="gray" textAlign="center">Create Post</Typography>
+       <UserBox>
+       <Avatar sx={{ width: '20' }} alt="Remy Sharp" src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+<Typography fontWeight={500}>Abdel Karim</Typography>
+       </UserBox>
+       <TextField
+       sx={{width:"100%"}}
+       id="standard-multiline-static"
+       multiline
+       rows={3}
+       placeholder="What's on your mind ?"
+       variant='standard'
+       />
+       <Stack direction="row" gap={1} mt={2} mb={3}>
+<EmojiEmotions color='primary'/>
+<Image color='secondary'/>
+<VideoCameraBack color='success'/>
+<PersonAdd color='error'/>
+       </Stack>
+       <ButtonGroup
+       fullWidth
+       variant="contained"
+       arial-label="outlined primary button group"
+       >
+        <Button>Post</Button>
+        <Button sx={{width:"100px"}}><DateRange/></Button>
+       </ButtonGroup>
+        </Box>
+    </StyledModal>
   </>
   );
 }
